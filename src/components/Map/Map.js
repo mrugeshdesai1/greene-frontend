87083b0566a6df31fe9787a1a3257fa8eca473d7
Mrugesh Desai <mrugesh.desai1@gmail.com>
2024-03-06 20:05:16 -0700

    troubleshoot the map component

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,23 +1,28 @@
 import React from 'react';
 import './Map.scss';
-import { GoogleMap, Marker, InfoWindow, LoadScript } from '@react-google-maps/api';
+import { GoogleMap, Marker, InfoWindow, useLoadScript } from '@react-google-maps/api';
 import axios from 'axios';
 import { useState , useEffect } from 'react';
 
 const mapContainerStyle = {
     width: '100%',
-    height: '400px',
+    height: '100%',
 };
 
 const center = {
     lat: 51.15,
     lng: -113.91,
-  };  
+  }; 
   
 
-const MAP_API = process.env.GOOGLE_MAP_API_KEY;
+const MAP_API = process.env.REACT_APP_GOOGLE_MAP_API_KEY;
 
 function Map() {
+
+
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: MAP_API,
+  });
 
     const [chargingStations, setChargingStations] = useState([]);
     const [selectedStation, setSelectedStation] = useState(null);
@@ -40,30 +45,34 @@ function Map() {
 
 
   return (
-    <LoadScript googleMapsApiKey= {MAP_API}>
-      <GoogleMap mapContainerStyle={mapContainerStyle} zoom={10} center={center}>
-        {chargingStations.map(station => (
-          <Marker
-            key={station.id}
-            position={{ lat: station.lat, lng: station.lng }}
-            onClick={() => setSelectedStation(station)}
-          />
-        ))}
+    <div className= 'greene__locations'>
+      {!isLoaded ? (
+        <h1 className= 'greene__locations-loading'>Loading...</h1>
+      ) : (
+        <GoogleMap className='greene__locations-mapcontainer' mapContainerStyle={mapContainerStyle} zoom={10} center={center}>
+          {chargingStations.map(station => (
+            <Marker 
+              key={station.id}
+              position={{ lat: station.lat, lng: station.lng }}
+              onClick={() => setSelectedStation(station)}
+            />
+          ))}
 
-        {selectedStation && (
-          <InfoWindow
-            position={{ lat: selectedStation.lat, lng: selectedStation.lng }}
-            onCloseClick={() => setSelectedStation(null)}
-        >
-        
-        <div>
-            <h3>{selectedStation.name}</h3>
-            <p>{selectedStation.address}</p>
-        </div>
-          </InfoWindow>
-        )}
-      </GoogleMap>
-    </LoadScript>
+          {selectedStation && (
+              <InfoWindow
+                  position={{ lat: selectedStation.lat, lng: selectedStation.lng }}
+                  onCloseClick={() => setSelectedStation(null)}
+                  >
+              
+                  <div greene__locations-stationinfo>
+                      <h3>{selectedStation.name}</h3>
+                      <p>{selectedStation.address}</p>
+                  </div>
+              </InfoWindow>
+          )}
+        </GoogleMap>
+      )}
+    </div>
   )
 }
 
