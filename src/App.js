c3993f950a3079c1334db6ba8f2712ac92c0c170
Mrugesh Desai <mrugesh.desai1@gmail.com>
2024-03-13 20:10:13 -0600

    create authentication and register pages

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import {BrowserRouter,Routes,Route} from 'react-router-dom';
 import Header from './components/Header/Header';
 import Home from './pages/Home/Home';
 import Footer from './components/Footer/Footer';
+import Authentication from './pages/Authentication/Authentication';
+import Register from './pages/Register/Register';
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
         <Header />
         <Routes>
           <Route path='/' element={<Home />}/>
+          <Route path='/authenticate' element={<Authentication />}/>
+          <Route path='/register' element={<Register />}/>
         </Routes>
         <Footer />
       </BrowserRouter>
---
+++ src/pages/Authentication/Authentication.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Authentication.scss'
+
+function Authentication() {
+  return (
+    <div>Authentication</div>
+  )
+}
+
+export default Authentication
